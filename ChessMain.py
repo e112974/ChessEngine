b00adb479dd059ee1790caa7df663607b4c941b3
Murat <64643767+e112974@users.noreply.github.com>
2021-10-14 16:04:52 +0100

    Update ChessMain.py

--- ChessMain.py
+++ ChessMain.py
@@ -1,42 +1,42 @@
+# import modules
+import ChessEngine
+import pygame
+
+# initialize pygame module
+pygame.init()
+
 #-------------------------------
 # import modules
 #-------------------------------
-from pygame.draw import rect
-from pygame.font import SysFont
-import ChessEngine
-import pygame as p
-
-p.init()
 BoardWidth = BoardHeight = 1024
 MoveLogPanelWidth = 400
 MoveLogPanelHeight = BoardHeight
 Dimension = 8
 SqSize = BoardHeight // Dimension
-MaxFPS = 15
 Images = {}
 
 
 def loadImages():
     pieces = ["B_R","B_N","B_B","B_Q","B_K","B_P","W_R","W_N","W_B","W_Q","W_K","W_P"]
     for piece in pieces:
-        Images[piece] = p.transform.scale(p.image.load('images/' + piece + '.png'),(SqSize,SqSize))
+        Images[piece] = pygame.transform.scale(pygame.image.load('images/' + piece + '.png'),(SqSize,SqSize))
 
 def main():
-    p.init()
-    screen = p.display.set_mode((BoardWidth+MoveLogPanelWidth,BoardHeight))
-    clock = p.time.Clock()
-    screen.fill(p.Color("white"))
-    moveLogFont = p.font.SysFont('Arial',24,False,False)
+    pygame.init()
+    screen = pygame.display.set_mode((BoardWidth+MoveLogPanelWidth,BoardHeight))
+    clock = pygame.time.Clock()
+    screen.fill(pygame.Color("white"))
+    moveLogFont = pygame.font.SysFont('Arial',24,False,False)
     gs = ChessEngine.GameState()
     loadImages()
     running = True
     while running:
-        for e in p.event.get():
-            if e.type == p.QUIT:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
                 running = False           
         DrawGameState(screen,gs,moveLogFont)
-
-
+        pygame.display.flip()
+        
 def DrawGameState(screen,gs,moveLogFont):
     DrawBoard(screen)
     DrawPieces(screen,gs.board)
@@ -44,18 +44,18 @@ def DrawGameState(screen,gs,moveLogFont):
         
 def DrawBoard(screen):
     global colors
-    colors = [p.Color("white"), p.Color("brown")]
+    colors = [pygame.Color("white"), pygame.Color("brown")]
     for row in range(Dimension):
         for col in range(Dimension):
             color = colors[((row+col) % 2)]
-            p.draw.rect(screen,color,p.Rect(col*SqSize,row*SqSize,SqSize,SqSize))
+            pygame.draw.rect(screen,color,pygame.Rect(col*SqSize,row*SqSize,SqSize,SqSize))
 
 def DrawPieces(screen,board):
     for row in range(Dimension):
         for col in range(Dimension):
             piece = board[row][col]
             if piece != "--":
-                screen.blit(Images[piece],p.Rect(col*SqSize,row*SqSize,SqSize,SqSize))
+                screen.blit(Images[piece],pygame.Rect(col*SqSize,row*SqSize,SqSize,SqSize))
 
 if __name__ == "__main__":
     main()
